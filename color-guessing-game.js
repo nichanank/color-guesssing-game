f8c7a51fb5e6b03b02a24073f349b3f623c1080b
Nichanan Kesonpat <nichanan.k@berkeley.edu>
2017-11-30 22:41:37 +0700

    implemented easy/hard difficulty options

--- color-guessing-game.js
+++ color-guessing-game.js
@@ -1,19 +1,51 @@
-var colors = generateRandomColor(6)
+var numSquares = 6
+var colors = generateRandomColors(6)
 var squares = document.querySelectorAll(".square")
 var pickedColor = pickColor()
 var colorDisplay = document.getElementById("colorDisplay")
 var messageDisplay = document.querySelector("#message")
 var h1 = document.querySelector("h1")
 var resetButton = document.querySelector("#reset")
+var easyBtn = document.querySelector("#easyBtn")
+var hardBtn = document.querySelector("#hardBtn")
+
+easyBtn.addEventListener("click", function() {
+    easyBtn.classList.add("selected")
+    hardBtn.classList.remove("selected")
+    numSquares = 3
+    colors = generateRandomColors(numSquares)
+    pickedColor = pickColor()
+    colorDisplay.textContent = pickedColor
+    for(var i = 0; i < squares.length; i++) {
+        if(colors[i]) {
+            squares[i].style.background = colors[i]
+        } else {
+            squares[i].style.display = "none"
+        }
+    }
+})
+
+hardBtn.addEventListener("click", function() {
+    hardBtn.classList.add("selected")
+    easyBtn.classList.remove("selected")
+    numSquares = 6
+    colors = generateRandomColors(numSquares)
+    pickedColor = pickColor()
+    colorDisplay.textContent = pickedColor
+    for(var i = 0; i < squares.length; i++) {
+        squares[i].style.background = colors[i]
+        squares[i].style.display = "block"
+    }
+})
 
 resetButton.addEventListener("click", function() {
-    colors = generateRandomColor(6)
+    colors = generateRandomColors(numSquares)
     pickedColor = pickColor()
     colorDisplay.textContent = pickedColor
     for(var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i]
     }
-    h1.style.backgroundColor = "#232323"
+    h1.style.backgroundColor = "steelblue"
 })
 
 colorDisplay.textContent = pickedColor
@@ -49,7 +81,7 @@ function pickColor() {
     return colors[random]
 }
 
-function generateRandomColor(num) {
+function generateRandomColors(num) {
     var arr = []
     for(var i = 0; i < num; i++) {
         arr.push(randomColor())
